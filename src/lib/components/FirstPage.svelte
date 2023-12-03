95a0b9a91b9244ec943e99b3273ea93fbac1e2ce
McLovin <mathieu.ford@gmail.com>
2023-12-03 17:07:14 -0500

    Added translations & placeholder pages

---
+++ src/lib/components/FirstPage.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { _, locale } from "svelte-i18n";
+
+	function changeLanguage() {
+		locale.update((locale) => (locale === "en" ? "fr" : "en"));
+	}
+</script>
+
+<header class="h-screen">
+	<div class="flex flex-col justify-between h-full p-3">
+		<div class="flex flex-row">
+			<p class="text-white underline text-3xl font-bold">450-922-2206</p>
+			<div class="flex-grow" />
+			<button class="btn variant-filled" on:click={changeLanguage}>
+				{$_("otherLanguage")}
+			</button>
+		</div>
+		<div class="flex flex-col">
+			<img class="max-w-lg m-auto" alt="RDI Logo" src="logo.png" />
+			<h1 class="text-4xl text-center text-white">{$_("landingPage.moto")}</h1>
+		</div>
+		<div>
+			<p class="text-white text-center text-2xl mb-3">{$_("landingPage.weRepare")}</p>
+			<div class="grid grid-cols-8 divide-x w-2/3 mx-auto">
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="skyjack-logo.png" alt="Skyjack Logo" />
+				</div>
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="genie-logo.png" alt="Genie Logo" />
+				</div>
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="imer-logo.png" alt="Imer Logo" />
+				</div>
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="jlg-logo.png" alt="JLG Logo" />
+				</div>
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="skytrak-logo.png" alt="Skytrak Logo" />
+				</div>
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="volvo-logo.svg" alt="Volvo Logo" />
+				</div>
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="cat-logo.png" alt="Cat Logo" />
+				</div>
+				<div class="flex justify-center items center">
+					<img class="w-28 h-12" src="jd-logo.png" alt="JD Logo" />
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,34 @@
 <script lang="ts">
-	let isFrench = true;
+	import FirstPage from "$lib/components/FirstPage.svelte";
+	import SecondPage from "$lib/components/SecondPage.svelte";
+	import { addMessages, getLocaleFromNavigator, init } from "svelte-i18n";
+	import en from "$lib/translations/en.json";
+	import fr from "$lib/translations/fr.json";
+	import ThirdPage from "$lib/components/ThirdPage.svelte";
+	import ContactUs from "$lib/components/ContactUs.svelte";
 
-	function changeLanguage() {
-		isFrench = !isFrench;
-	}
+	addMessages("en", en);
+	addMessages("fr", fr);
+
+	init({
+		fallbackLocale: "fr",
+		initialLocale: getLocaleFromNavigator()
+	});
 </script>
 
-<header class="h-screen bg-[url('skytrak1.jpg')] bg-cover bg-center">
-	<div class="bg-surface-600/[0.7] h-full">
-		<div class="flex flex-col justify-between h-full p-3">
-			<div class="flex flex-row">
-				<p class="text-white underline text-3xl font-bold">450-922-2206</p>
-				<div class="flex-grow" />
-				<button class="btn variant-filled" on:click={changeLanguage}>
-					{isFrench ? 'Anglais' : 'French'}
-				</button>
-			</div>
-			<div class="flex flex-col">
-				<img class="max-w-lg m-auto" alt="RDI Logo" src="logo.png" />
-				<h1 class="text-4xl text-center text-white">Réparation | Diagnostique | Inspection</h1>
-			</div>
-			<div>
-				<p class="text-white text-center text-2xl mb-3">Nous réparons</p>
-				<div class="grid grid-cols-8 divide-x w-2/3 mx-auto">
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="skyjack-logo.png" alt="Skyjack Logo" />
-					</div>
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="genie-logo.png" alt="Genie Logo" />
-					</div>
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="imer-logo.png" alt="Imer Logo" />
-					</div>
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="jlg-logo.png" alt="JLG Logo" />
-					</div>
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="skytrak-logo.png" alt="Skytrak Logo" />
-					</div>
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="volvo-logo.svg" alt="Volvo Logo" />
-					</div>
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="cat-logo.png" alt="Cat Logo" />
-					</div>
-					<div class="flex justify-center items center">
-						<img class="w-28 h-12" src="jd-logo.png" alt="JD Logo" />
-					</div>
-				</div>
-			</div>
+<div class="h-screen l bg-[url('skytrak1.jpg')] bg-cover bg-center">
+	<div class="bg-surface-600/[0.7] h-full snap-y snap-mandatory overflow-scroll">
+		<div class="snap-center">
+			<FirstPage />
+		</div>
+		<div class="snap-center">
+			<SecondPage />
+		</div>
+		<div class="snap-center">
+			<ThirdPage />
+		</div>
+		<div class="snap-center">
+			<ContactUs />
 		</div>
 	</div>
-</header>
+</div>
