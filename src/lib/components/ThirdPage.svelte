26691dd9e4f9aee57125e97033800e5b51fbe1cd
McLovin <mathieu.ford@gmail.com>
2023-12-03 19:18:22 -0500

    Added slideshow to second page

--- src/lib/components/ThirdPage.svelte
+++ src/lib/components/ThirdPage.svelte
@@ -1,9 +1,61 @@
 <script lang="ts">
 	import { _ } from "svelte-i18n";
+	import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+	import "@splidejs/svelte-splide/css";
 </script>
 
-<section class="h-screen">
-	<div class="flex flex-col justify-center h-full">
-		<h2 class="text-6xl text-white text-center">{$_("secondPage.services")}</h2>
+<section class="h-screen flex flex-col justify-center">
+	<div
+		class="h-2/3 bg-surface-600 rounded-xl w-2/3 mx-auto flex flex-col justify-center text-white"
+	>
+		<Splide
+			options={{
+				autoplay: true,
+				pauseOnFocus: true,
+				rewind: true
+			}}
+			class="h-full flex flex-col justify-center"
+		>
+			<SplideSlide>
+				<div class="flex flex-row justify-center w-2/3 mx-auto">
+					<h1 class="text-4xl font-bold text-center">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit omnis commodi, non,
+						aspernatur similique amet soluta necessitatibus voluptatem expedita quam iste dolore
+						reiciendis dignissimos unde saepe id! Odio, possimus.
+					</h1>
+					<img src="placeholder.jpg" alt="placeholder" />
+				</div>
+			</SplideSlide>
+			<SplideSlide>
+				<div class="flex flex-row justify-center w-2/3 mx-auto">
+					<h1 class="text-4xl font-bold text-center">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit omnis commodi, non,
+						aspernatur similique amet soluta necessitatibus voluptatem expedita quam iste dolore
+						reiciendis dignissimos unde saepe id! Odio, possimus.
+					</h1>
+					<img src="placeholder.jpg" alt="placeholder" />
+				</div>
+			</SplideSlide>
+			<SplideSlide>
+				<div class="flex flex-row justify-center w-2/3 mx-auto">
+					<h1 class="text-4xl font-bold text-center">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit omnis commodi, non,
+						aspernatur similique amet soluta necessitatibus voluptatem expedita quam iste dolore
+						reiciendis dignissimos unde saepe id! Odio, possimus.
+					</h1>
+					<img src="placeholder.jpg" alt="placeholder" />
+				</div>
+			</SplideSlide>
+			<SplideSlide>
+				<div class="flex flex-row justify-center w-2/3 mx-auto">
+					<h1 class="text-4xl font-bold text-center">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. A impedit omnis commodi, non,
+						aspernatur similique amet soluta necessitatibus voluptatem expedita quam iste dolore
+						reiciendis dignissimos unde saepe id! Odio, possimus.
+					</h1>
+					<img src="placeholder.jpg" alt="placeholder" />
+				</div>
+			</SplideSlide>
+		</Splide>
 	</div>
 </section>
